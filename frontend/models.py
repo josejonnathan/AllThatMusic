9880d31e0f85664d75e6063c747efd570522acdc
Jonnathan Gonzalez <jonnathan@origenaudio.com>
2024-04-22 17:04:46 +0200

    readme updated

--- frontend/models.py
+++ frontend/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+
 class Artist(models.Model):
     """
     Model to store information about music artists.
--- frontend/urls.py
+++ frontend/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import HomePageView, DeezerRequestView, ArtistListView, ArtistDetailView, AlbumListView, AlbumDetailView, SongListView, SongDetailView, ArtistUpdateView, SongUpdateView
 
 urlpatterns = [
